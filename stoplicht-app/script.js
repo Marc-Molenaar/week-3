60603292c51d90d3afef5a8974c2485c39e46a95
Marc Molenaar <marcmolenaar2004@icloud.com>
2023-09-12 10:31:05 +0200

    improved stoplicht-app

--- stoplicht-app/script.js
+++ stoplicht-app/script.js
@@ -1,10 +1,18 @@
 async function start() {
 
     $(".light").css("background-color", "green")
+    $("html").css("background-color", "red")
+    $(".light").css("margin-left", "20px")
     await sleep(5);
+
     $(".light").css("background-color", "red")
+    $("html").css("background-color", "yellow")
+    $(".light").css("margin-left", "80px")
     await sleep(5);
+
     $(".light").css("background-color", "yellow")
+    $("html").css("background-color", "green")
+    $(".light").css("margin-left", "0px")
     await sleep(5);
 
     start()
@@ -13,3 +21,7 @@ async function start() {
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+$(document).ready(function() {
+    start()
+});
