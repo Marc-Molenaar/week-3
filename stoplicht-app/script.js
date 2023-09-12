f0c486cff797497e1154452844f1969437e0f43e
Marc Molenaar <marcmolenaar2004@icloud.com>
2023-09-12 10:03:10 +0200

    opdrachten

--- stoplicht-app/script.js
+++ stoplicht-app/script.js
@@ -6,6 +6,18 @@ async function start() {
     await sleep(20);
     $(".light").css("background-color", "yellow")
     await sleep(20);
+    $(".light").css("background-color", "white")
+    await sleep(20);
+    $(".light").css("background-color", "black")
+    await sleep(20);
+    $(".light").css("background-color", "indigo")
+    await sleep(20);
+    $(".light").css("background-color", "purple")
+    await sleep(20);
+    $(".light").css("background-color", "pink")
+    await sleep(20);
+    $(".light").css("background-color", "gray")
+    await sleep(20);
 
     start()
 }
